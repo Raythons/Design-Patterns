c7d87df2adb426f64548339a87589671c8886bd1
mustafa haider <mustafahaider351@gmail.com>
2024-11-01 20:19:39 +0300

    Refactoring: Moved creating people logic into new class called FakeDatabase

--- Program.cs
+++ Program.cs
@@ -5,33 +5,5 @@
 
 MailManager mailManager = new EmaiMailManager();
 MailManager sms = new EmaiMailManager();
-
-List<Person> people = new();
-
-for (int i = 0; i < 30; i++)
-{
-    string name = $"Person {i + 1}";
-    string identifier = i % 2 == 0 ? $"person{i}@example.com" : $"555-123-{i:0000}";
-    string password = "password123";
-    decimal payPerHour = 25.0M;
-
-    people.Add(new Teacher(name, identifier, password, payPerHour));
-    people.Add(new Teacher(name, identifier, password, payPerHour));
-    people.Add(new Teacher(name, identifier, password, payPerHour));
-    people.Add(new Teacher(name, identifier, password, payPerHour));
-    people.Add(new Teacher(name, identifier, password, payPerHour));
-    people.Add(new Teacher(name, identifier, password, payPerHour));
-    people.Add(new Teacher(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-    people.Add(new Coach(name, identifier, password, payPerHour));
-
-}
-
+var people = FakeDatabase.FillDataBase();
 MailManager.SendMails(people);
