dd84b8ba87d8c9dcd212bfbeb901fc3285903efe
mustafa haider <mustafahaider351@gmail.com>
2024-11-03 20:42:05 +0300

    Added Prototype design pattern

---
+++ Prototype/CloneablePrototype.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace FactoryMethod.Prototype
+{
+    public abstract class CloneablePrototype<T> : IPrototype<T> where T : class
+    {
+        public T Clone()
+        {
+            return (T)MemberwiseClone();
+        }
+        public T DeepClone()
+        {
+            var serialized = JsonSerializer.Serialize(this);
+            return JsonSerializer.Deserialize<T>(serialized)!;
+        }
+    }
+}
