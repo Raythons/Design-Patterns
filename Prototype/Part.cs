146d0e6d25f5d249af31cd3d8cbb5ab38ba5fc7d
mustafa haider <mustafahaider351@gmail.com>
2024-11-07 15:40:09 +0300

    unnecessary commit

---
+++ Prototype/Part.cs
@@ -0,0 +1,14 @@
+﻿namespace FactoryMethod.Prototype
+{
+    public class Part : ICloneable
+    {
+        public double Price { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public bool IsBroken { get; set; }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype/PrototypeMethods.cs
@@ -0,0 +1,22 @@
+﻿namespace FactoryMethod.Prototype
+{
+    public static class PrototypeMethods
+    {
+        public static void Test()
+        {
+            var car = new Car();
+            var part = new Part { IsBroken = false, Name = "Engine", Price = 2000 };
+            car.Parts.Add(part);
+            var car2 = car.DeepCopy();
+            car.Parts[0].Name = "Wheel";
+            foreach (var p in car.Parts)
+            {
+                Console.WriteLine("car1: " + p.Name);
+            }
+            foreach (var p in car2.Parts)
+            {
+                Console.WriteLine("car2: " + p.Name);
+            }
+        }
+    }
+}
